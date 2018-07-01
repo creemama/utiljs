1169e73ae35e397ac1d386bc54a4cd748fd15868
Chris Topher <c@creemama.com>
2018-07-01 17:38:24 -0400

    Rearrange the methods in numbers

--- packages/utiljs-numbers/Numbers.js
+++ packages/utiljs-numbers/Numbers.js
@@ -3,9 +3,11 @@
 
 module.exports = function Numbers() {
 	this.floor = Math.floor
-	this.isInt = isInt
-	this.parseInt = Number.parseInt
 	this.random = Math.random
+
+	this.parseInt = Number.parseInt
+
+	this.isInt = isInt
 
 	// http://stackoverflow.com/questions/14636536/how-to-check-if-a-variable-is-an-integer-in-javascript
 	function isInt(value) {
