58ee4efc2981148f2d0fa54d4288cc186bb103ef
Chris Topher <c@creemama.com>
2018-07-02 01:58:53 -0400

    Remove Objects_ as a prefix to method names in Objects

--- packages/utiljs-objects/Objects.js
+++ packages/utiljs-objects/Objects.js
@@ -3,25 +3,25 @@
 module.exports = Objects;
 
 function Objects() {
-  this.guarantee = Objects_guaranteeObject;
-  this.isDefined = Objects_isDefined;
+  this.guarantee = guarantee;
+  this.isDefined = isDefined;
   this.keys = Object.keys;
-  this.merge = Objects_merge;
+  this.merge = merge;
 
   // This method is useful when determining
   // if an integer variable is defined or not,
   // since 0 is false and may be valid.
-  function Objects_isDefined(obj) {
+  function isDefined(obj) {
     return obj !== null && typeof obj !== "undefined";
   }
 
-  function Objects_guaranteeObject(obj) {
-    return Objects_isDefined(obj) ? obj : {};
+  function guarantee(obj) {
+    return isDefined(obj) ? obj : {};
   }
 
-  function Objects_merge(a, b) {
-    var c = Objects_guaranteeObject(a);
-    var d = Objects_guaranteeObject(b);
+  function merge(a, b) {
+    var c = guarantee(a);
+    var d = guarantee(b);
     var merged = {};
     for (var p in c) if (c.hasOwnProperty(p)) merged[p] = c[p];
     for (var p in d) if (d.hasOwnProperty(p)) merged[p] = d[p];
