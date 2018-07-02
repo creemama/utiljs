b5a78550c04598a1701a68fcd186c3c4352b3266
Chris Topher <c@creemama.com>
2018-07-02 00:46:57 -0400

    Disregard lerna.json when running prettier
    since the formatting when running the lerna publish command is
    different from the formatting of JSON files run through prettier.

--- scripts/prettier.sh
+++ scripts/prettier.sh
@@ -10,6 +10,7 @@ find . -type f \
 | egrep "^.*\.(css|js|json|md)$" \
 | egrep -v "^.*/(node_modules|target)/.*$" \
 | egrep -v "^.*/package-lock\.json$" \
+| egrep -v "^\./lerna\.json$" \
 > prettier.txt
 
 while read in; do npx prettier --write "$in"; done < prettier.txt
