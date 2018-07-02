7933a3ee49fd8f41667d7027c139592e5a50711a
Chris Topher <c@creemama.com>
2018-07-02 00:39:59 -0400

    Add Bash, current-directory boilerplate to prettier.sh

--- prettier.sh
+++ prettier.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+curDir=`pwd`
+scriptDir=`dirname "${0}"`
+cd "${scriptDir}"
+
 find . -type f \
 | egrep "^.*\.(css|js|json|md)$" \
 | egrep -v "^.*/(node_modules|target)/.*$" \
@@ -9,3 +13,5 @@ find . -type f \
 while read in; do npx prettier --write "$in"; done < prettier.txt
 
 rm -rf prettier.txt
+
+cd "${curDir}"
