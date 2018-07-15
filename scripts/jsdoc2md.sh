b38501db7674e37d6a8a7780854ccc7284712019
Chris Topher <c@creemama.com>
2018-07-15 04:33:45 +0000

    Update jsdoc2md.sh with better output filenames

--- scripts/jsdoc2md.sh
+++ scripts/jsdoc2md.sh
@@ -9,19 +9,23 @@ cd ..
 # Put jsdoc on the PATH.
 PATH=`pwd`/node_modules/.bin:$PATH
 
-if [ -f "target/jsdoc2md.md" ]; then
-  rm -rf target/jsdoc2md.md
+if [ -d "target/jsdoc2md" ]; then
+  rm -rf target/jsdoc2md
 fi
 
+mkdir -p target/jsdoc2md
+
 prefix=""
+filename="jsdoc2md.md"
 if [ ! -z "${1}" ] && [ -d "packages/${1}" ]; then
   prefix="\./packages/${1}/"
+  filename="${1}.md"
 fi
 
 jsdoc2md \
 `find . -type f \
 | egrep "^${prefix}.*\.js$" \
 | egrep -v "^.*/(dist|node_modules|target)/.*$"` \
-> target/jsdoc2md.md
+> target/jsdoc2md/${filename}
 
 cd "${curDir}"
