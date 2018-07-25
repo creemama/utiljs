99f3b4ff1526adfb608e1aa66bc69e0a38591cdc
Chris Topher <c@creemama.com>
2018-07-25 07:29:03 -0400

    Alphabetize functions in Numbers

--- packages/utiljs-numbers/lib/Numbers.js
+++ packages/utiljs-numbers/lib/Numbers.js
@@ -1,24 +1,6 @@
 "use strict";
 
 module.exports = function Numbers() {
-  this.E = Math.E;
-  this.LN10 = Math.LN10;
-  this.LN2 = Math.LN2;
-  this.LOG10E = Math.LOG10E;
-  this.LOG2E = Math.LOG2E;
-  this.PI = Math.PI;
-  this.SQRT1_2 = Math.SQRT1_2;
-  this.SQRT2 = Math.SQRT2;
-
-  this.EPSILON = Number.EPSILON;
-  this.MAX_SAFE_INTEGER = Number.MAX_SAFE_INTEGER;
-  this.MAX_VALUE = Number.MAX_VALUE;
-  this.MIN_SAFE_INTEGER = Number.MIN_SAFE_INTEGER;
-  this.MIN_VALUE = Number.MIN_VALUE;
-  this.NEGATIVE_INFINITY = Number.NEGATIVE_INFINITY;
-  this.NaN = Number.NaN;
-  this.POSITIVE_INFINITY = Number.POSITIVE_INFINITY;
-
   this.abs = Math.abs;
   this.acos = Math.acos;
   this.acosh = Math.acosh;
@@ -38,12 +20,27 @@ module.exports = function Numbers() {
   this.fround = Math.fround;
   this.hypot = Math.hypot;
   this.imul = Math.imul;
+  this.isFinite = Number.isFinite;
+  this.isInt = isInt;
+  // http://stackoverflow.com/questions/14636536/how-to-check-if-a-variable-is-an-integer-in-javascript
+  function isInt(value) {
+    return (
+      !isNaN(value) &&
+      parseInt(Number(value)) == value &&
+      !isNaN(parseInt(value, 10))
+    );
+  }
+  this.isInteger = Number.isInteger;
+  this.isNaN = Number.isNaN;
+  this.isSafeInteger = Number.isSafeInteger;
   this.log = Math.log;
   this.log10 = Math.log10;
   this.log1p = Math.log1p;
   this.log2 = Math.log2;
   this.max = Math.max;
   this.min = Math.min;
+  this.parseFloat = Number.parseFloat;
+  this.parseInt = Number.parseInt;
   this.pow = Math.pow;
   this.random = Math.random;
   this.round = Math.round;
@@ -55,21 +52,20 @@ module.exports = function Numbers() {
   this.tanh = Math.tanh;
   this.trunc = Math.trunc;
 
-  this.isFinite = Number.isFinite;
-  this.isInteger = Number.isInteger;
-  this.isNaN = Number.isNaN;
-  this.isSafeInteger = Number.isSafeInteger;
-  this.parseFloat = Number.parseFloat;
-  this.parseInt = Number.parseInt;
-
-  this.isInt = isInt;
-
-  // http://stackoverflow.com/questions/14636536/how-to-check-if-a-variable-is-an-integer-in-javascript
-  function isInt(value) {
-    return (
-      !isNaN(value) &&
-      parseInt(Number(value)) == value &&
-      !isNaN(parseInt(value, 10))
-    );
-  }
+  this.E = Math.E;
+  this.EPSILON = Number.EPSILON;
+  this.LN10 = Math.LN10;
+  this.LN2 = Math.LN2;
+  this.LOG10E = Math.LOG10E;
+  this.LOG2E = Math.LOG2E;
+  this.MAX_SAFE_INTEGER = Number.MAX_SAFE_INTEGER;
+  this.MAX_VALUE = Number.MAX_VALUE;
+  this.MIN_SAFE_INTEGER = Number.MIN_SAFE_INTEGER;
+  this.MIN_VALUE = Number.MIN_VALUE;
+  this.NEGATIVE_INFINITY = Number.NEGATIVE_INFINITY;
+  this.NaN = Number.NaN;
+  this.PI = Math.PI;
+  this.POSITIVE_INFINITY = Number.POSITIVE_INFINITY;
+  this.SQRT1_2 = Math.SQRT1_2;
+  this.SQRT2 = Math.SQRT2;
 };
