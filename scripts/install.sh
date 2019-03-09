72d3bc8f7769144203749e9d21e19491deb0a2c2
Chris Topher <c@creemama.com>
2019-03-08 19:40:45 -0500

    build: update scripts/install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-curDir=`pwd`
 scriptDir=`dirname "${0}"`
 cd "${scriptDir}"
 
@@ -9,13 +8,11 @@ cd ..
 npm install --save-exact
 exitCode=${?}
 npx lerna bootstrap -- --save-exact
-if [[ ${?} -ne 0 ]]; then
+if [ ${?} -ne 0 ]; then
   exitCode=${?}
 fi
 
 printf "\n\033[1m%s\033[0m\n\n" "** Consider running \"npm run package-lock\" as well. **"
-
-cd "${curDir}"
 
 echo "Exit code: ${exitCode}"
 exit ${exitCode}
