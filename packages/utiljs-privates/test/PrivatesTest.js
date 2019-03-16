0e4d5c1d1637d69ae50d82314233ed0a23e17fd2
Chris Topher <c@creemama.com>
2019-03-16 14:19:07 +0000

    test: add a test for Privates#call

--- packages/utiljs-privates/test/PrivatesTest.js
+++ packages/utiljs-privates/test/PrivatesTest.js
@@ -82,3 +82,84 @@ describe("Privates", () => {
     expect(example.getProperty2()).to.eql(2);
   });
 });
+
+describe("Privates#call", () => {
+  const privates = new Privates();
+  it("should correctly call privates that are functions", () => {
+    const thiz = {};
+
+    // Test a lambda function.
+    privates.set(thiz, "lambda", () => "lambdaObj");
+    expect(privates.call(thiz, "lambda")).to.eql("lambdaObj");
+
+    // Test a declared function.
+    function func() {
+      return "funcObj";
+    }
+    privates.set(thiz, "function", func);
+    expect(privates.call(thiz, "function")).to.eql("funcObj");
+
+    // Test a class function.
+    class CallClass {
+      delegate() {
+        return "classObj";
+      }
+      call() {
+        return this.delegate();
+      }
+    }
+    const c = new CallClass();
+    privates.set(thiz, "classBound", c.call.bind(c));
+
+    // Test an object as a property.
+    const key = { a: "a" };
+    privates.set(thiz, key, () => "objectAsProperty");
+    expect(privates.call(thiz, key)).to.eql("objectAsProperty");
+
+    // Test a symbol as a property.
+    const symbol = Symbol("symbol");
+    privates.set(thiz, symbol, () => "symbolAsProperty");
+    expect(privates.call(thiz, symbol)).to.eql("symbolAsProperty");
+
+    // Test a number as a property.
+    privates.set(thiz, -5, () => "numberAsProperty");
+    expect(privates.call(thiz, -5)).to.eql("numberAsProperty");
+
+    // Test a boolean as a property.
+    privates.set(thiz, false, () => "booleanAsProperty");
+    expect(privates.call(thiz, false)).to.eql("booleanAsProperty");
+
+    // Test undefined as a property.
+    privates.set(thiz, undefined, () => "undefinedAsProperty");
+    expect(privates.call(thiz, undefined)).to.eql("undefinedAsProperty");
+
+    // Test null as a property.
+    privates.set(thiz, null, () => "nullAsProperty");
+    expect(privates.call(thiz, null)).to.eql("nullAsProperty");
+  });
+  it("should fail with invalid arguments", () => {
+    const thiz = {};
+
+    expect(() => privates.call()).to.throw(RethrownError);
+    expect(() => privates.call(null, "nonexistent")).to.throw(RethrownError);
+    expect(() => privates.call(thiz)).to.throw(RethrownError);
+    expect(() => privates.call(thiz, "nonexistent")).to.throw(RethrownError);
+
+    // Test an unbound function failing.
+    class CallClass {
+      delegate() {
+        return "classObj";
+      }
+      call() {
+        return this.delegate();
+      }
+    }
+    const c = new CallClass();
+    privates.set(thiz, "classBound", c.call);
+    expect(() => privates.call(thiz, "classBound")).to.throw(RethrownError);
+
+    // Test a private that is not a function.
+    privates.set(thiz, "notafunction", "string");
+    expect(() => privates.call(thiz, "notafunction")).to.throw(RethrownError);
+  });
+});
