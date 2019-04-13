2cf8f60409017c49db46a4724861d776a10eaa6a
Chris Topher <c@creemama.com>
2019-04-13 11:03:59 -0400

    docs: remove dead comments in scripts/install-globals.sh

--- scripts/install-globals.sh
+++ scripts/install-globals.sh
@@ -8,10 +8,6 @@ fi
 
 # We use these global packages to run in Travis CI.
 
-# Including @babel/core prevents the following warning:
-# npm WARN @babel/cli@7.2.3 requires a peer of @babel/core@^7.0.0-0 but
-# none is installed. You must install peer dependencies yourself.
-
 npm install --global npm@6.9.0 \
 && npm install --global \
   lerna@3.13.2 \
