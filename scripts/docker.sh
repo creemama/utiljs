40e88a6279ba6a00420d4e72cf1f6879f92b00c4
Chris Topher <c@creemama.com>
2019-04-12 21:11:29 -0400

    chore: update the development Dockerfile

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -1,15 +1,14 @@
 #!/bin/sh
 
+set -o errexit -o nounset
+IFS="$(printf '\n\t' '')"
+if [ -n "${BASH_VERSION:-}" ]; then
+  set -o pipefail
+fi
+
 SCRIPT_DIR="$( cd "$(dirname "$0")" ; pwd -P )"
 
-# We mount a directory for /home/node to get around permissions problems. If we
-# had mounted /home/node/utiljs without mounting a directory for /home/node,
-# then the root user would own /home/node preventing us from writing to it.
-NODE_HOME_DIR="${SCRIPT_DIR}"/../target/node-home-dir
-
-mkdir -p "${NODE_HOME_DIR}"
-
-DOCKER_IMAGE=utiljs-dev:0.36.0
+DOCKER_IMAGE=utiljs-dev:0.36.1
 
 # https://stackoverflow.com/a/30543453
 if [[ "$(docker images -q ${DOCKER_IMAGE} 2> /dev/null)" == "" ]]; then
@@ -34,14 +33,20 @@ fi
 # ...
 
 docker run \
---interactive \
---name utiljs-dev \
---read-only \
---rm \
---tty \
---volume "${NODE_HOME_DIR}":/home/node \
---volume ~/.ssh:/home/node/.ssh:ro \
---volume "${SCRIPT_DIR}"/..:/home/node/utiljs \
---volume /tmp \
---workdir /home/node/utiljs \
-${DOCKER_IMAGE}
+  --cap-drop=ALL \
+  --cpu-shares=1024 \
+  --interactive \
+  --memory 1.5G \
+  --name utiljs-dev \
+  --pids-limit 100 \
+  --read-only \
+  --restart=no \
+  --rm \
+  --security-opt=no-new-privileges:true \
+  --tty \
+  --volume /home/node \
+  --volume ~/.ssh:/home/node/.ssh:ro \
+  --volume "${SCRIPT_DIR}/..:/home/node/utiljs" \
+  --volume /tmp \
+  --workdir /home/node/utiljs \
+  "${DOCKER_IMAGE}"
