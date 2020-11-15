fb51dc6a1697b6f12a56b1dac09f760c1b3a3c84
Chris Topher <c@creemama.com>
2020-11-15 05:25:13 -0500

    test: update ErrorsTest with more test cases
    adding TODO items for buggy stacks and messages.

--- packages/utiljs-errors/test/ErrorsTest.js
+++ packages/utiljs-errors/test/ErrorsTest.js
@@ -12,6 +12,7 @@ describe("Errors#catch(promise)", () => {
     return errors.catch(rejectAPromise()).catch((error) => {
       expect(error).to.be.an.instanceof(AsyncError);
       expect(error.message).to.eql("Fail!");
+      expect(error.original).to.be.an.instanceof(TypeError);
       expect(error.stack).to.include("AsyncError: Fail!");
       expect(error.stack).to.include("at rejectAPromise");
       expect(error.stack).to.include("TypeError: Fail!");
@@ -24,9 +25,46 @@ describe("Errors#catch(promise)", () => {
     return errors.catch(rejectAPromise(), "optional message").catch((error) => {
       expect(error).to.be.an.instanceof(AsyncError);
       expect(error.message).to.eql("optional message");
+      expect(error.original).to.be.an.instanceof(TypeError);
       expect(error.stack).to.include("AsyncError: optional message");
       expect(error.stack).to.include("at rejectAPromise");
       expect(error.stack).to.include("TypeError: Fail!");
+    });
+  });
+  it("should return a Promise that throws an AsyncError upon rejecting with a string", () => {
+    function rejectAPromise() {
+      return Promise.reject("ENTITY_NOT_FOUND");
+    }
+    return errors.catch(rejectAPromise()).catch((error) => {
+      expect(error).to.be.an.instanceof(AsyncError);
+      expect(error.message).to.eql("");
+      expect(error.original).to.eql("ENTITY_NOT_FOUND");
+      expect(error.stack).to.include("AsyncError");
+      expect(error.stack).to.include("ENTITY_NOT_FOUND");
+    });
+  });
+  it("should return a Promise that throws an AsyncError upon rejecting with an array", () => {
+    function rejectAPromise() {
+      return Promise.reject([{ code: "1234" }]);
+    }
+    return errors.catch(rejectAPromise()).catch((error) => {
+      expect(error).to.be.an.instanceof(AsyncError);
+      expect(error.message).to.eql("");
+      expect(error.original).to.eql([{ code: "1234" }]);
+      expect(error.stack).to.include("AsyncError");
+      // TODO BUG: This should not be [object Object]. It should be a string representation of [{"code":"1234"}].
+      expect(error.stack).to.include("[object Object]");
+    });
+  });
+  it("should return a Promise that throws an AsyncError upon rejecting with a circularly referenced object", () => {
+    const obj = {};
+    obj.circular = obj;
+    return errors.catch(Promise.reject(obj)).catch((error) => {
+      expect(error).to.be.an.instanceof(AsyncError);
+      expect(error.message).to.eql("");
+      expect(error.original).to.eql(obj);
+      expect(error.stack).to.include("AsyncError");
+      expect(error.stack).to.include("[object Object]");
     });
   });
   it("should throw a TypeError if the given promise is not defined", () => {
@@ -56,14 +94,87 @@ describe("new Errors#RethrownError(message, error)", () => {
       rethrowTheTypeError();
       expect.fail("We expected the runtime to throw a RethrownError.");
     } catch (error) {
-      if (error instanceof AssertionError) throw error;
-      expect(error).to.be.an.instanceof(RethrownError);
+      if (!(error instanceof RethrownError)) throw error;
+      expect(error.message).to.eql("Lorem Ipsum");
       expect(error.original).to.be.an.instanceof(TypeError);
       expect(error.stack).to.include("RethrownError: Lorem Ipsum");
       expect(error.stack).to.include("at rethrowTheTypeError");
       expect(error.stack).to.include("TypeError: Invalid Argument");
       expect(error.stack).to.include("at throwATypeError");
     }
+  });
+  it("should create a rethrown error from a thrown string", () => {
+    function throwAString() {
+      throw "ENTITY_NOT_FOUND";
+    }
+    function rethrowTheString() {
+      try {
+        throwAString();
+      } catch (error) {
+        expect(new RethrownError(error).stack).to.include("RethrownError\n");
+        // TODO BUG: This should not be undefined.
+        expect(new RethrownError(error).stack).to.include("undefined");
+        expect(new RethrownError(error, "").stack).to.include(
+          "RethrownError\n"
+        );
+        expect(new RethrownError(error, 0).stack).to.include("RethrownError\n");
+        // TODO BUG: This should not be undefined.
+        expect(new RethrownError(error, "").stack).to.include("undefined");
+        throw new RethrownError(error, "Lorem Ipsum");
+      }
+    }
+    try {
+      rethrowTheString();
+      expect.fail("We expected the runtime to throw a RethrownError.");
+    } catch (error) {
+      if (!(error instanceof RethrownError)) throw error;
+      expect(error.message).to.eql("Lorem Ipsum");
+      expect(error.original).to.eql("ENTITY_NOT_FOUND");
+      expect(error.stack).to.include("RethrownError");
+      expect(error.stack).to.include("at rethrowTheString");
+      // TODO BUG: This should not be undefined.
+      expect(error.stack).to.include("undefined");
+    }
+  });
+  it("should create a rethrown error from a thrown array", () => {
+    function throwAnArray() {
+      throw [{ code: "1234" }];
+    }
+    function rethrowTheArray() {
+      try {
+        throwAnArray();
+      } catch (error) {
+        expect(new RethrownError(error).stack).to.include("RethrownError\n");
+        // TODO BUG: This should not be undefined.
+        expect(new RethrownError(error).stack).to.include("undefined");
+        expect(new RethrownError(error, "").stack).to.include(
+          "RethrownError\n"
+        );
+        // TODO BUG: This should not be undefined.
+        expect(new RethrownError(error, "").stack).to.include("undefined");
+        throw new RethrownError(error, "Lorem Ipsum");
+      }
+    }
+    try {
+      rethrowTheArray();
+      expect.fail("We expected the runtime to throw a RethrownError.");
+    } catch (error) {
+      if (!(error instanceof RethrownError)) throw error;
+      expect(error.message).to.eql("Lorem Ipsum");
+      expect(error.original).to.eql([{ code: "1234" }]);
+      expect(error.stack).to.include("RethrownError");
+      expect(error.stack).to.include("at rethrowTheArray");
+      // TODO BUG: This should not be undefined.
+      expect(error.stack).to.include("undefined");
+    }
+  });
+  it("should create a rethrown error from a circularly referenced object", () => {
+    const obj = {};
+    obj.circular = obj;
+    const error = new RethrownError(obj);
+    expect(error.stack).to.include("RethrownError\n");
+    // TODO BUG: This should not be undefined.
+    expect(error.stack).to.include("undefined");
   });
   it("should throw an error when given an invalid error", () => {
     expect(() => new RethrownError()).to.throw(TypeError);
