e4f4020ac843b5a932b2e821edbcea79f556c2a1
Chris Topher <c@creemama.com>
2020-11-15 17:23:52 -0500

    fix: change @babel/runtime-corejs2 to @babel/runtime-corejs3
    ```
    npm WARN deprecated core-js@2.6.11: core-js@<3 is no longer maintained and not recommended for usage due to the number of issues. Please, upgrade your dependencies to the actual version of core-js@3.
    ```

--- babel.config.js
+++ babel.config.js
@@ -3,7 +3,7 @@ module.exports = function (api) {
   api.cache(true);
 
   const presets = ["@babel/preset-env"];
-  const plugins = [["@babel/plugin-transform-runtime", { corejs: 2 }]];
+  const plugins = [["@babel/plugin-transform-runtime", { corejs: 3 }]];
 
   return {
     presets,
